6813076c73acb1eaff75fa9fccd3ab616eaff0f3
Jimin <kaley0421@naver.com>
2023-11-02 19:20:10 +0900

    Init: 프로젝트 초기 셋팅

---
+++ src/main/java/com/kw/LinkIt/LinkItApplication.java
@@ -0,0 +1,13 @@
+package com.kw.LinkIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkItApplication.class, args);
+	}
+
+}
