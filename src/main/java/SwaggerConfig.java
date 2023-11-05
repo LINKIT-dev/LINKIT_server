f5b7faf352a00323ec655f57606cdd79e4cec2c0
SeungHo0422 <songseungho9258@gmail.com>
2023-11-06 02:33:15 +0900

    feat: openapi swagger 의존성 추가 및 설정 (#6)

---
+++ src/main/java/SwaggerConfig.java
@@ -0,0 +1,14 @@
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "LINKIT API Documentation",
+                description = "LINKIT API 명세 페이지입니다.",
+                version = "v1"
+        )
+)
+@Configuration
+public class SwaggerConfig {
+}
--- src/main/java/com/kw/LinkIt/LinkItApplication.java
+++ src/main/java/com/kw/LinkIt/LinkItApplication.java
@@ -2,8 +2,12 @@ package com.kw.LinkIt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * DataSourceAutoConfiguration : DB 설정 시 삭제 후 application.properties 지정해줘야 한다.
+ */
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class LinkItApplication {
 
 	public static void main(String[] args) {
