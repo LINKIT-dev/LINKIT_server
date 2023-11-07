91a0226c2166c1e5d21d576525e17ddeaa65879e
Jimin <kaley0421@naver.com>
2023-11-07 23:24:57 +0900

    Style: config 폴더 생성

--- src/main/java/com/kw/LinkIt/global/config/SwaggerConfig.java
+++ src/main/java/com/kw/LinkIt/global/config/SwaggerConfig.java
@@ -1,3 +1,5 @@
+package com.kw.LinkIt.global.config;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
