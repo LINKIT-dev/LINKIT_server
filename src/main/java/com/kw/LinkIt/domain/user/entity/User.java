0bc3f3870cc7b49fc073f32fceab85be23b85c31
SeungHo0422 <songseungho9258@gmail.com>
2023-11-22 22:18:14 +0900

    feat: userDetails 추가 및 Repository 추가 (#14)

--- src/main/java/com/kw/LinkIt/domain/user/entity/User.java
+++ src/main/java/com/kw/LinkIt/domain/user/entity/User.java
@@ -1,14 +1,19 @@
 package com.kw.LinkIt.domain.user.entity;
 
 import jakarta.persistence.*;
+import java.util.Collection;
+import java.util.HashSet;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -16,7 +21,53 @@ public class User {
 
     private String uid;
 
+    private String password;
+
     private String profileImg;
 
     private Long oauthId;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<GrantedAuthority>();
+    }
+
+    @Override
+    public String getUsername() {
+        return id.toString();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Builder
+    public User(String uid, String password, Long oauthId) {
+        this.uid = uid;
+        this.password = password;
+        this.oauthId = oauthId;
+    }
+
+
 }
