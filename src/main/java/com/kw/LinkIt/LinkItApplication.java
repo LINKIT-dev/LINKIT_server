31469594cbab2b5e5dedbeac1a16dd4127606a24
SeungHo0422 <songseungho9258@gmail.com>
2023-11-22 22:20:22 +0900

    chore: xml.bind dependency 추가 및 Main Annotation 수정 (#14)

--- src/main/java/com/kw/LinkIt/LinkItApplication.java
+++ src/main/java/com/kw/LinkIt/LinkItApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * DataSourceAutoConfiguration : DB 설정 시 삭제 후 application.properties 지정해줘야 한다.
  */
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaAuditing
 @EnableFeignClients
 public class LinkItApplication {
 
