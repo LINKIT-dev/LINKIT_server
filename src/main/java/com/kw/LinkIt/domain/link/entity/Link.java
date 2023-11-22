e106da86a6c634ec4f3f5f2b606c02a93374d3c8
Jimin <kaley0421@naver.com>
2023-11-22 17:43:04 +0900

    Fix: Link Entity 에서 liked 필드 제거 (#18)

--- src/main/java/com/kw/LinkIt/domain/link/entity/Link.java
+++ src/main/java/com/kw/LinkIt/domain/link/entity/Link.java
@@ -24,8 +24,6 @@ public class Link {
 
     private String previewImg;
 
-    private Integer liked;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
