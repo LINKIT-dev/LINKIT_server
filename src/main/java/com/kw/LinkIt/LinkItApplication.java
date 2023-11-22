b935987ff4af7e8eb8fe7ea9c2d4410bfba47f1b
Jimin <kaley0421@naver.com>
2023-11-22 19:21:09 +0900

    Feat: @SpringBootApplication 에서 DataSourceAutoConfiguration 제거

--- src/main/java/com/kw/LinkIt/LinkItApplication.java
+++ src/main/java/com/kw/LinkIt/LinkItApplication.java
@@ -2,12 +2,8 @@ package com.kw.LinkIt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-/**
- * DataSourceAutoConfiguration : DB 설정 시 삭제 후 application.properties 지정해줘야 한다.
- */
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class LinkItApplication {
 
 	public static void main(String[] args) {
