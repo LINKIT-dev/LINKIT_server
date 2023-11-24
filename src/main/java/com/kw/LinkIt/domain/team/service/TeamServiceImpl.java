1c2433de9dcc1459c97dec965b6dad223654843e
Jimin <kaley0421@naver.com>
2023-11-24 10:42:28 +0900

    Fix: '팀 생성' 시 UserTeam 매핑 테이블 엔티티도 함께 생성하도록 수정

--- src/main/java/com/kw/LinkIt/domain/team/service/TeamServiceImpl.java
+++ src/main/java/com/kw/LinkIt/domain/team/service/TeamServiceImpl.java
@@ -5,6 +5,8 @@ import com.kw.LinkIt.domain.team.entity.Team;
 import com.kw.LinkIt.domain.team.repository.TeamRepository;
 import com.kw.LinkIt.domain.user.entity.User;
 import com.kw.LinkIt.domain.user.repository.UserRepository;
+import com.kw.LinkIt.domain.userTeam.entity.UserTeam;
+import com.kw.LinkIt.domain.userTeam.repository.UserTeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +17,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class TeamServiceImpl implements TeamService {
 
     private final TeamRepository teamRepository;
+    private final UserTeamRepository userTeamRepository;
+
     public void createTeam(CreateTeamDTO createTeamDTO, User user) {
-        teamRepository.save(Team.builder()
+        Team savedTeam = teamRepository.save(Team.builder()
                         .name(createTeamDTO.getName())
                         .profileImg(createTeamDTO.getProfileImgUrl())
                         .capacity(createTeamDTO.getCapacity())
                         .leader(user)
                         .build());
+        userTeamRepository.save(UserTeam.builder()
+                .user(user)
+                .team(savedTeam)
+                .build());
     }
 
     public Team getTeam(Long teamId) {
